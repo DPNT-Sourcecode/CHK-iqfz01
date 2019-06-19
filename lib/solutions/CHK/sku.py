c4929527763e458ddf93e12e25a4c9d0dd8b6a22
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 10:45:44 +0000

    Wed Jun 19 10:45:44 UTC 2019

--- lib/solutions/CHK/sku.py
+++ lib/solutions/CHK/sku.py
@@ -1,5 +1,7 @@
 class SKU:
 
-    def __init__(self, sku_id, price):
+    def __init__(self, sku_id, price, num_discount=None, discounted_price=None):
         self.sku_id = sku_id
         self.price = price
+        self.discounted_price = discounted_price
+        self.num_discount = num_discount
