613f06dd5fbc210c80e6270a0796005e7597602f
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 10:54:44 +0000

    Wed Jun 19 10:54:44 UTC 2019

---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,12 @@
+from solutions.CHK.sku import SKU
+import unittest
+
+class TestSum(unittest.TestCase):
+
+    def test_sku(self, *args, **kwargs):
+        
+        sku = SKU('A', 50, 3, 130)
+        sku = SKU('A', 50, 3, 130)
+        sku = SKU('A', 50, 3, 130)
+        sku = SKU('A', 50, 3, 130)
+        
