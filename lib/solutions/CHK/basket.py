f39322beb67440b22679ef6fe396f1bb5bf6f948
sbx_user1051 <sbx_user1051@169.254.123.197>
2019-06-19 12:33:34 +0000

    Wed Jun 19 12:33:34 UTC 2019

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -20,8 +20,8 @@ class Basket:
 
         num_a = order_string.count('A')
         a_discount_one = num_a // 5 #quantity divisible by 5, for first discount
-        a_discount_two = (num_a - (num_a % 5)) // 3 # quantity div. by 3 for second discount
-        a_remain = num_a - (a_discount_one * 5) - a_discount_two * 3  # remaining quantity for leftover pricing
+        a_discount_two = (a_discount_one) // 3 # quantity div. by 3 for second discount
+        a_remain = num_a % 15  # remaining quantity for leftover pricing
         total += (
             a_discount_one  * 200
         ) + 
@@ -31,11 +31,19 @@ class Basket:
         # do e next so we can discount b more easily
         num_e = order_string.count('E')
         free_b = num_e % 2
+        total += num_e * 40
+
+        # b
+        num_b = order_string.count('B') - free_b
+        total += (
+            (num_b // 2)
+        )
 
         # we don't need to return -1 anymore, as any string is valid
         # in this implementation. We could reject srings not containing
         # SKU IDs we possess, but the spec does not ask for this
         # so behaviour may be unexpected.
         return total
+
 
 
