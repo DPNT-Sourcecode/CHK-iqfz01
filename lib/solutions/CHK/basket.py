d270fcabeecaded5caaa202d17dbd29cc4a445e9
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 11:24:44 +0000

    Wed Jun 19 11:24:44 UTC 2019

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -4,13 +4,16 @@ class Basket:
 
         for sku in sku_list:
             self.add_sku(sku)
-            
+
     def add_sku(self, sku):
         self.price_table[sku.sku_id] = sku
 
+    def incr_sku(self, sku_id):
+        self.price_table[sku_id].quantity += 1
 
     def get_total(self):
         total = 0
         for sku in self.price_table.values():
             total += sku.get_price()
         return total
+
--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,19 +4,20 @@
 def checkout(skus):
     from .sku import SKU # need to use relative import because we don't know the structure of  
                      # the test suite.   
-    price_table = {
-      'A': SKU('A', 50, 3, 130),
-      'B': SKU('B', 30, 2, 45),
-      'C': SKU('C', 20),
-      'D': SKU('D', 15)
-    } 
-        
+    from .basket import Basket
+    sku_list = [
+      SKU('A', 50, 3, 130),
+      SKU('B', 30, 2, 45),
+      SKU('C', 20),
+      SKU('D', 15)
+    ]
+    basket = Basket(sku_list)
     # There is no example input which made this all but guarantee a penalty.
-    total = 0
     for char in skus:
         try:
             # The spec does not mention quantity so assume it is 1.
-            total += price_table.get(char).get_price()
-        except TypeError:
+            basket.incr_sku(char)
+        except TypeError, KeyError:
             return -1
-    return total
+    return basket.get_total()
+
