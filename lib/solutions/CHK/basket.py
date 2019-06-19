c1f4fb6aa5d33fd4a2a1873d6dd6ad5a959ee6c2
sbx_user1051 <sbx_user1051@169.254.123.197>
2019-06-19 12:36:34 +0000

    Wed Jun 19 12:36:34 UTC 2019

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -11,39 +11,45 @@ class Basket:
     def incr_sku(self, sku_id):
         self.price_table[sku_id].quantity += 1
 
-    def get_total(self, order_string):
-        total = 0
-        # the previous implementation got a little complicated,
-        # so for expediency we will just calculate very simply
-        # until we know or can limit the precise format of our discounts
-        # which is simpler from an operational standpoint and for the customer.
+def get_total(self, order_string):
+    total = 0
+    # the previous implementation got a little complicated,
+    # so for expediency we will just calculate very simply
+    # until we know or can limit the precise format of our discounts
+    # which is simpler from an operational standpoint and for the customer.
 
-        num_a = order_string.count('A')
-        a_discount_one = num_a // 5 #quantity divisible by 5, for first discount
-        a_discount_two = (a_discount_one) // 3 # quantity div. by 3 for second discount
-        a_remain = num_a % 15  # remaining quantity for leftover pricing
-        total += (
-            a_discount_one  * 200
-        ) + 
-            a_discount_two *  150
-        ) + (a_remain * 50)
+    num_a = order_string.count('A')
+    a_discount_one = num_a // 5 #quantity divisible by 5, for first discount
+    a_discount_two = a_discount_one // 3 # quantity div. by 3 for second discount
+    a_remain = num_a % 15  # remaining quantity for leftover pricing
+    total += (
+        a_discount_one  * 200
+    ) + 
+        a_discount_two *  150
+    ) + (a_remain * 50)
 
-        # do e next so we can discount b more easily
-        num_e = order_string.count('E')
-        free_b = num_e % 2
-        total += num_e * 40
+    # do e next so we can discount b more easily
+    num_e = order_string.count('E')
+    free_b = num_e % 2
+    total += num_e * 40
 
-        # b
-        num_b = order_string.count('B') - free_b
-        total += (
-            (num_b // 2)
-        )
+    # b
+    num_b = order_string.count('B') - free_b
+    total += (
+        (num_b // 2) * 45
+    ) + (num_b % 2 * 30))
 
-        # we don't need to return -1 anymore, as any string is valid
-        # in this implementation. We could reject srings not containing
-        # SKU IDs we possess, but the spec does not ask for this
-        # so behaviour may be unexpected.
-        return total
+    # c and d
+    total += order_string.count('C') * 15
+    total += order_string.count('D') * 20
+
+
+    # we don't need to return -1 anymore, as any string is valid
+    # in this implementation. We could reject srings not containing
+    # SKU IDs we possess, but the spec does not ask for this
+    # so behaviour may be unexpected.
+    return total
+
 
 
 
--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,21 +2,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    from .sku import SKU # need to use relative import because we don't know the structure of  
-                     # the test suite.   
-    from .basket import Basket
-    sku_list = [
-      SKU('A', 50, 3, 130),
-      SKU('B', 30, 2, 45),
-      SKU('C', 20),
-      SKU('D', 15)
-    ]
-    basket = Basket(sku_list)
-    # There is no example input which made this all but guarantee a penalty.
-    for char in skus:
-        try:
-            # The spec does not mention quantity so assume it is 1.
-            basket.incr_sku(char)
-        except (TypeError, KeyError):
-            return -1
-    return basket.get_total()
+    # need to use relative import because we don't know the structure of  
+    # the test suite.   
+    from .basket import get_total
+    
+    return get_total(skus)
+
