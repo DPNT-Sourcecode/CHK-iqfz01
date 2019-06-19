c549ca904f17740e5618eb82e05b98ff6d6787c6
sbx_user1051 <sbx_user1051@169.254.123.197>
2019-06-19 12:15:34 +0000

    Wed Jun 19 12:15:34 UTC 2019

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -14,6 +14,25 @@ class Basket:
     def get_total(self):
         total = 0
         for sku in self.price_table.values():
+            if self.discounts.get(sku.sku_id):
+                total += self.discounts[sku.sku_id].apply_discount()
             total += sku.get_price()
         return total
 
+
+
+    def apply_discounts(self, quantity=None):
+        if not quantity:
+            quantity = self.quantity
+        try:
+            quantity = int(quantity)
+        except ValueError:
+            return -1
+        remaining = quantity
+        total = 0
+        for discount in self.discounts:
+            total += (quantity // discount.required_quantity) * discount.price
+            remaining -= quantity % discount.required_quantity 
+        if remaining > 0:
+            total += (remaining * self.price)
+        return total
