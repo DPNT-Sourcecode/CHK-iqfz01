72ca5e2e17743d411064363afe2e2af55ac0ac28
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 11:09:44 +0000

    Wed Jun 19 11:09:44 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,17 @@
 def checkout(skus):
     from .sku import SKU # need to use relative import because we don't know the structure of  
                      # the test suite.   
-
+    price_table = {
+      'A': SKU('A', 50, 3, 130),
+      'B': SKU('B', 30, 2, 45),
+      'C': SKU('C', 20),
+      'D': SKU('D', 15)
+    } 
+        
     # There is no example input which made this all but guarantee a penalty.
+    total = '0'
     for char in skus:
+
 
 
 
