b56bbc67610d7c16f4744936df760c21b38db232
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 11:00:44 +0000

    Wed Jun 19 11:00:44 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,7 +1,7 @@
 from solutions.CHK.sku import SKU
 import unittest
 
-class TestSum(unittest.TestCase):
+class TestCheckout(unittest.TestCase):
 
     def test_sku(self, *args, **kwargs):
         
@@ -10,6 +10,20 @@ class TestSum(unittest.TestCase):
         c = SKU('C', 20)
         d = SKU('D', 15)
 
+        self.assertEqual(a.get_price(0), 0)
+        self.assertEqual(a.get_price(1), 50) 
         self.assertEqual(a.get_price(3), 130)
         self.assertEqual(b.get_price(2), 45)
+        self.assertEqual(a.get_price(4), 180)
+        self.assertEqual(a.get_price(6), 260)
+        self.assertEqual(b.get_price(3), 75)
+        self.assertEqual(c.get_price(10), 200)
+        self.assertEqual(d.get_price(3), 45)
+        self.assertEqual(a.get_price('kitties'), -1)
+        
+        
+        
+        
+        
+        
         
