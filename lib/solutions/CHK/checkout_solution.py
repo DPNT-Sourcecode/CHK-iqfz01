35908fa61ff22a6f4d64b71956c9c3efcedcf7c3
sbx_user1051 <sbx_user1051@169.254.86.77>
2019-06-19 11:06:44 +0000

    Wed Jun 19 11:06:44 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,12 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    from .sku import SKU # need to use relative import because we don't know the structure of  
+                     # the test suite.   
+
     # There is no example input which made this all but guarantee a penalty.
-    raise NotImplementedError()
+    for char in skus:
+
+
 
